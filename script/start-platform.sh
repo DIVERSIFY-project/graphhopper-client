8a5d38d632d2c9e3a2e3e18d8dc562b67d19abf9
Simon Allier <simon.allier@gmail.com>
2015-09-04 11:44:26 +0200

    update

--- script/start-platform.sh
+++ script/start-platform.sh
@@ -6,6 +6,6 @@ for host in $(cat $1)
     for i in $(seq 0 $2)
         do
         echo $i
-        ssh -Y ubuntu@$host sudo docker run -d -p 153$i:8080 songhui/smhp-final-14
+        ssh -Y ubuntu@$host sudo docker run -d -p 153$i:8080 songhui/smhp-final-14 
     done
 done
