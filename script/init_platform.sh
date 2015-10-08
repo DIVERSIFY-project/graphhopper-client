d9b8e16bfe05234659f51d70c0576852a58e9c76
Andre Elie <andre.elie@inria.fr>
2015-10-08 16:03:54 +0200

    script update (kill non functional)

--- script/init_platform.sh
+++ script/init_platform.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 if [ -z "$1" ]
   then
     echo "No hosts file supplied"
@@ -12,19 +13,23 @@ fi
 
 rm -f host_ip_list
 
-for i in $(seq 1 $2)
-    do
-    echo $i
-    for host in $(cat $1)
+for host in $(cat $1)
+do
+    echo $host
+    if [[ $host ==  10.0.0.* ]]
+    then
+        echo "Cloud computer"
+        ssh -t -o "ProxyCommand ssh fox@10.0.0.1 nc %h 22" diversify@$host sudo ./ghdemo_script $2
+        for i in $(seq 1 $2)
         do
-        echo $host
-        #token=$(ssh -Y ubuntu@$host sudo docker run -d -p 153$i:8080 songhui/smhp-final-14)
-        #ssh -Y diversify@$host sudo docker run -d -p 153$i:8080 songhui/smhp-final-14 &
-        #ssh -Y diversify@$host sudo docker run -d -p 153$i:8080 songhui/smhp-hopper-jetty &
-        #ssh -Y diversify@$host sudo docker run -d -p 153$i:8080 barais/undertowsmartgh &
-        ssh -Y diversify@$host sudo docker run -d -p 153$i:8080 aelie/diversify &
-        echo $host:153$i >> host_ip_list
-        #echo $token >> token_list
-    done
-    sleep 1s
+            echo $host:153$i >> host_ip_list
+        done
+    else
+        for i in $(seq 1 $2)
+        do
+            ssh -Y diversify@$host sudo docker run -d -p 153$i:8080 aelie/diversify-light
+            echo $host:153$i >> host_ip_list
+        done
+    fi
 done
+
